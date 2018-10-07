f5bae9255c4cecb318cfb7540985cbb928616df8
fcamblor <fcamblor@gmail.com>
2018-10-07 22:40:07 +0200

    computed map and field map should be considered optional

--- src/ts-object-transformer.spec.ts
+++ src/ts-object-transformer.spec.ts
@@ -1,7 +1,8 @@
 import {transformObject} from './ts-object-transformer';
+import {keys} from 'lodash';
 
 describe("ts-object-transformer", () => {
-    it("json mappings", () => {
+    it("object transformation", () => {
         let transformedResult = transformObject(
             // Standard plain object literal coming, most of the time from serverside, generally described by an interface
             // Let's call this type SRC
@@ -29,18 +30,104 @@ describe("ts-object-transformer", () => {
         //   mappedResult = { date: Date.parse("2018-10-04T00:00:00+0200"), date2: new Date(1538604000000), aString: unescape("Hello%20World"), idempotentValue: "foo", computed: "Hello%20World__foo" }
         // ..  meaning that expected type would be { date: number, date2: Date, aString: string, idempotentValue: string, computed: string }
 
-        let v1: number = transformedResult.date;
+        expect(keys(transformedResult)).toEqual(['date', 'date2', 'aString', 'idempotentValue', 'computed']);
+
+        let v1: number = transformedResult.date; // number, expected
         expect(typeof v1).toEqual('number');
-        let v2 = transformedResult.date2; // Date, expected
+        expect(v1).toEqual(1538604000000);
+        let v2: Date = transformedResult.date2; // Date, expected
         expect(typeof v2).toEqual('object');
         expect(v2).toBeInstanceOf(Date);
-        let v3 = transformedResult.aString; // string, expected
+        expect(v2.getTime()).toEqual(1538604000000);
+        let v3: string = transformedResult.aString; // string, expected
         expect(typeof v3).toEqual('string');
-        let v4 = transformedResult.idempotentValue; // string, expected
+        expect(v3).toEqual('Hello World');
+        let v4: string = transformedResult.idempotentValue; // string, expected
         expect(typeof v4).toEqual('string');
-        let v5 = transformedResult.computed; // string, expected
+        expect(v4).toEqual('foo');
+        let v5: string = transformedResult.computed; // string, expected
         expect(typeof v5).toEqual('string');
+        expect(v5).toEqual('Hello%20World__foo');
 
         // transformedResult.blah // doesn't compile, Property 'blah' doesn't exist on type
+    });
+
+    it("object transformation with no computed values", () => {
+        let transformedResult = transformObject(
+            { date: "2018-10-04T00:00:00+0200", date2: 1538604000000, aString: "Hello%20World", idempotentValue: "foo" },
+            { date: Date.parse, date2: (ts: number) => new Date(ts), aString: unescape }
+        );
+
+        expect(keys(transformedResult)).toEqual(['date', 'date2', 'aString', 'idempotentValue']);
+
+        let v1: number = transformedResult.date; // number, expected
+        expect(typeof v1).toEqual('number');
+        expect(v1).toEqual(1538604000000);
+        let v2: Date = transformedResult.date2; // Date, expected
+        expect(typeof v2).toEqual('object');
+        expect(v2).toBeInstanceOf(Date);
+        expect(v2.getTime()).toEqual(1538604000000);
+        let v3: string = transformedResult.aString; // string, expected
+        expect(typeof v3).toEqual('string');
+        expect(v3).toEqual('Hello World');
+        let v4: string = transformedResult.idempotentValue; // string, expected
+        expect(typeof v4).toEqual('string');
+        expect(v4).toEqual('foo');
+        // let v5: string = transformedResult.computed; // doesn't compile, property 'computed' doesn't exist on type
+    });
+
+    function jsonMappingsWithFieldMappings(fieldMapping: {}|undefined) {
+        let transformedResult = transformObject(
+            { date: "2018-10-04T00:00:00+0200", date2: 1538604000000, aString: "Hello%20World", idempotentValue: "foo" },
+            fieldMapping,
+            { computed: (_, obj) => `${obj?`${obj.aString}__${obj.idempotentValue}`:''}` }
+        );
+
+        expect(keys(transformedResult)).toEqual(['date', 'date2', 'aString', 'idempotentValue', 'computed']);
+
+        let v1: string = transformedResult.date; // string expected
+        expect(typeof v1).toEqual('string');
+        expect(v1).toEqual('2018-10-04T00:00:00+0200');
+        let v2: number = transformedResult.date2; // number, expected
+        expect(typeof v2).toEqual('number');
+        expect(v2).toEqual(1538604000000);
+        let v3: string = transformedResult.aString; // string, expected
+        expect(typeof v3).toEqual('string');
+        expect(v3).toEqual('Hello%20World');
+        let v4: string = transformedResult.idempotentValue; // string, expected
+        expect(typeof v4).toEqual('string');
+        expect(v4).toEqual('foo');
+        let v5: string = transformedResult.computed; // string, expected
+        expect(typeof v5).toEqual('string');
+        expect(v5).toEqual('Hello%20World__foo');
+    }
+    it("object transformation with no field mappings", () => {
+        jsonMappingsWithFieldMappings({});
+    });
+
+    it("object transformation with undefined field mappings", () => {
+        jsonMappingsWithFieldMappings(undefined);
+    });
+
+    it("idempotent transformation", () => {
+        let transformedResult = transformObject(
+            { date: "2018-10-04T00:00:00+0200", date2: 1538604000000, aString: "Hello%20World", idempotentValue: "foo" }
+        );
+
+        expect(keys(transformedResult)).toEqual(['date', 'date2', 'aString', 'idempotentValue']);
+
+        let v1: string = transformedResult.date; // string expected
+        expect(typeof v1).toEqual('string');
+        expect(v1).toEqual('2018-10-04T00:00:00+0200');
+        let v2: number = transformedResult.date2; // number, expected
+        expect(typeof v2).toEqual('number');
+        expect(v2).toEqual(1538604000000);
+        let v3: string = transformedResult.aString; // string, expected
+        expect(typeof v3).toEqual('string');
+        expect(v3).toEqual('Hello%20World');
+        let v4: string = transformedResult.idempotentValue; // string, expected
+        expect(typeof v4).toEqual('string');
+        expect(v4).toEqual('foo');
+        // let v5: string = transformedResult.computed; // doesn't compile, property 'computed' doesn't exist on type
     });
 });
--- src/ts-object-transformer.ts
+++ src/ts-object-transformer.ts
@@ -1,4 +1,4 @@
-import {reduce, has, get} from "lodash";
+import {has, each, keys} from "lodash";
 
 
 // Thanks to Titian Cernicova-Dragomir https://stackoverflow.com/a/52641402/476345
@@ -11,42 +11,42 @@ type ComputedMap<SRC> = {
 } & {
     [ATTR: string]: (value: undefined, obj: SRC) => any
 };
-type MappedReturnType<SRC, FM extends FieldMap<SRC>, CM extends ComputedMap<SRC>> = {
-    [ATTR in keyof CM]: ReturnType<CM[ATTR]>
-} & {
-    [ATTR in keyof SRC]: ATTR extends keyof FM
+type MappedReturnType<SRC, FM extends FieldMap<SRC>|undefined, CM extends ComputedMap<SRC>|undefined> =
+    (CM extends ComputedMap<SRC> ? { [ATTR in keyof CM]: ReturnType<CM[ATTR]> } : unknown )
+    & { [ATTR in keyof SRC]: ATTR extends keyof FM
         ? FM[ATTR] extends (value: SRC[ATTR], obj?: SRC) => infer R ? R : SRC[ATTR]
         : SRC[ATTR]
-}
+    }
 
 export function transformObject<
-    SRC extends object, FM extends FieldMap<SRC>, CM extends ComputedMap<SRC>
->(src: SRC, fieldMap: FM, computedMap: CM): MappedReturnType<SRC, FM, CM> {
-    let result = reduce(src, (result: any, value: any, key: string) => {
+    SRC extends object, FM extends FieldMap<SRC>|undefined, CM extends ComputedMap<SRC>|undefined
+>(src: SRC, fieldMap?: FM, computedMap?: CM): MappedReturnType<SRC, FM, CM> {
+    let result: any = {};
+    each(<(keyof SRC)[]> keys(src), (key) => {
+        let value: any = src[key];
         if(fieldMap && keyOfSrc<SRC, FM, CM>(fieldMap, key)) {
             let transformer = <(value: any, obj?: SRC) => any>fieldMap[key];
             result[key] = transformer(value, src);
         } else {
             result[key] = value;
         }
-        return result;
-    }, <any>{});
+    });
 
     if(computedMap) {
-        result = reduce(computedMap, (result: any, value: any, key: keyof CM) => {
-            let transformer = <(value: undefined, obj: SRC) => any>computedMap[key];
-            result[key] = transformer(value, src);
+        each(<(keyof CM)[]> keys(computedMap), (key) => {
+            let transformer = <(value: undefined, obj: SRC) => any>computedMap[<string>key];
+            result[key] = transformer(undefined, src);
             return result;
-        }, result);
+        });
     }
 
     return result;
 }
 
-function keyOfSrc<SRC extends object, FM extends FieldMap<SRC>, CM extends ComputedMap<SRC>>(fieldMap: FM|undefined, key: (keyof SRC | keyof CM)): key is keyof SRC {
+function keyOfSrc<SRC extends object, FM extends FieldMap<SRC>|undefined, CM extends ComputedMap<SRC>|undefined>(fieldMap: FM, key: (keyof SRC | keyof CM)): key is keyof SRC {
     return has(fieldMap, key);
 }
 
-function keyOfComputedMap<SRC extends object, FM extends FieldMap<SRC>, CM extends ComputedMap<SRC>>(computedMap: CM|undefined, key: (keyof SRC | keyof CM)): key is keyof CM {
+function keyOfComputedMap<SRC extends object, FM extends FieldMap<SRC>, CM extends ComputedMap<SRC>>(computedMap: CM, key: (keyof SRC | keyof CM)): key is keyof CM {
     return has(computedMap, key);
 }
